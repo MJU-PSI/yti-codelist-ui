e688de3f60ea5477e681f556fb3a9ab2ada2fe90
Tadej Šivak <tadej.sivak@gora.si>
2023-08-17 11:59:36 +0200

    setting base href in index.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ envsubst '${CODELIST_API_URL},${CODELIST_INTAKE_URL},${TERMINOLOGY_API_URL},${ME
 if [[ $ANGULAR_BASE_HREF != "/" ]] # * is used for pattern matching
 then
   ANGULAR_BASE_HREF="${ANGULAR_BASE_HREF}/";
-  sed -i 's#<base href="./">#<base href="'"${ANGULAR_BASE_HREF}"'">#' /usr/share/nginx/html/index.html
+  sed -i 's#<base href="/">#<base href="'"${ANGULAR_BASE_HREF}"'">#' /usr/share/nginx/html/index.html
 fi
 
 # Start nginx
